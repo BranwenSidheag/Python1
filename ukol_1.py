72255e2910ee7a9196d730ef247083797ec9c53f
Michaela Bulvova <michaela.bulvova@email.cz>
2023-01-18 18:02:08 +0100

    Ukol_1

---
+++ ukol_1.py
@@ -0,0 +1,12 @@
+# Úkol
+jmeno = "Michaela"
+domena = "@czechitas.cz"
+
+print(jmeno + domena)
+
+# Bonus
+jmeno_a_prijmeni = input("Zadejte jméno a příjmení: ")
+
+print(jmeno_a_prijmeni.upper())
+print(jmeno_a_prijmeni.lower())
+
