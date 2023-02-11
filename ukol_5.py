26d9e64ee62fb1884a5034174305f67cd7ba9992
Michaela Bulvova <michaela.bulvova@email.cz>
2023-02-11 16:45:05 +0100

    Úkol 5

--- ukol_5.py
+++ ukol_5.py
@@ -19,3 +19,19 @@ print(nocni_teploty)
 
 poledne_noc = [(teplota[1], teplota[3]) for teplota in teploty]
 print(poledne_noc)
+
+# Bonus
+teploty = [
+    ["pondělí", 2.1, 5.2, 6.1, -0.1],
+    ["úterý", 2.2, 4.8, 5.6, -1.0],
+    ["středa", 3.3, 6.5, 5.9, 1.2],
+    ["čtvrtek", 2.9, 5.6, 6.0, 0.0],
+    ["pátek", 2.0, 4.6, 5.5, -1.2],
+    ["sobota", 1.0, 3.2, 2.1, -2.0],
+    ["neděle", 0.4, 2.7, 1.3, -2.8]
+]
+
+teploty2 = [(teplota[0], (sum(teplota[1:])/4)) for teplota in teploty]
+
+denni_teploty = {den: prumerna_teplota for den, prumerna_teplota in teploty2}
+print(denni_teploty)
